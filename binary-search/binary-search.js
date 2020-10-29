5f797cfcbd8a7e28a5f49bbe7043a6d574de663c
Noelle Laureano <nrlaureano@gmail.com>
2020-10-29 14:14:19 -0400

    Binary Search: updates mocha version; makes formatting consistent

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,17 +1,17 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (arr, target) => {
+
 };
 
 /*
-	EXTRA CREDIT:
+  EXTRA CREDIT:
 
-	Can you augment the function above to run in constant O(1) space?
-	This means, you cannot edit/copy the original array!
-	How can we use other pieces of data, like pointers, to avoid slicing?
+  Can you augment the function above to run in constant O(1) space?
+  This means, you cannot edit/copy the original array!
+  How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
