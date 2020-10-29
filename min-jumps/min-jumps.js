2d18bdf6a8a9b390adae98c8ed07b48419b1100d
Noelle Laureano <nrlaureano@gmail.com>
2020-10-29 14:36:09 -0400

    Adds solution code and extra credit tests for findLoop

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,18 @@
 
 // Complete this algo
 const minJumps = (arr) => {
+  let jumps = arr.map((el) => Infinity);
+  jumps[0] = 0;
 
+  for (let i = 1; i < jumps.length; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (j + arr[j] >= i) {
+        jumps[i] = Math.min(jumps[i], jumps[j] + 1);
+      }
+    }
+  }
+
+  return jumps[arr.length - 1];
 };
 
 module.exports = minJumps;
